69815cbf38a8f7c2fd4c84a9f0d2ea2fb62fb3a2
21013885 <21013885@DESKTOP-9LSJ9OL>
2022-08-04 00:39:14 +0800

    created casestudy class

---
+++ C206_CaseStudy/src/CaseStudy.java
@@ -0,0 +1,24 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * LEW EN QUAN 21013885, 28/7/2022 10:10AM
+ */
+
+/**
+ * @author 21013885
+ *
+ */
+import java.util.ArrayList;
+public class CaseStudy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
